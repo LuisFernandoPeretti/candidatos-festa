3ca8061343c17ab3fc67809b1ac09ee1c0012d6c
Luis Fernando <nando.lfp@hotmail.com>
2022-01-19 11:01:59 -0300

    backup

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react'
+import './App.css';
+
+//components
+import EntradaCadastro from './components/EntradaCadastro';
+
+function App() {
+  return (
+    <Fragment>
+      <div className='container'>
+        <EntradaCadastro />
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
